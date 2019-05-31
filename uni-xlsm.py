5d6dc14abb2c598b0b1f39868501074104588362
Eduardo Ungaratto <eduardo.ungarattolima@gmail.com>
2019-05-31 15:15:55 -0300

    new version

--- uni-xlsm.py
+++ uni-xlsm.py
@@ -1,34 +1,29 @@
+import os
 import pandas as pd
+import xlrd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
-#FILE 1
-df1 = pd.read_excel('C:\Users\DELL\Downloads\TESTE_UNIFICAR\ARQR_1.xlsm');
-xl1 = ExcelFile('C:\Users\DELL\Downloads\TESTE_UNIFICAR\ARQR_1.xlsm');
-xl1.sheet_names
-sht1 = str(xl1.sheet_names[0]);
-df1 = pd.read_excel('C:\Users\DELL\Downloads\TESTE_UNIFICAR\ARQR_1.xlsm', sheet_name=sht1, index_col=0);
-print("Column headings:");
-print(df1.columns);
+try:
+	dir = os.fsencode(".\\<your path>");
+	df = pd.DataFrame();
 
-#FILE 2
-df2 = pd.read_excel('C:\Users\DELL\Downloads\TESTE_UNIFICAR\ARQR_2.xlsm');
-xl2 = ExcelFile('C:\Users\DELL\Downloads\TESTE_UNIFICAR\ARQR_2.xlsm');
-xl2.sheet_names
-sht2 = str(xl2.sheet_names[0]);
-df2 = pd.read_excel('C:\Users\DELL\Downloads\TESTE_UNIFICAR\ARQR_1.xlsm', sheet_name=sht2, index_col=0, columns=list(''));
-print("Column headings:");
-print(df2.columns);
+	for file in os.listdir(dir):
+		fxls = str(os.fsdecode(dir) + '\\' + os.fsdecode(file));
+		newname = fxls.replace(".XLS", ".xlsm");
+		os.rename(fxls, newname);
+		fxls = newname;
 
-print df1.columns == df2.columns;
-sheet = sht1 if sht1 == sht2 else '';
+		fxls = xlrd.open_workbook(fxls, encoding_override="cp1252")
+		sheet = str(ExcelFile(fxls).sheet_names[0]);
+		
+		os.system('cls');
+		print("Working on this file: " + str(newname));
+		exc = pd.read_excel(fxls, sheet_name=sheet);
+		df = df.append(exc);
 
-#NEW FILE
-output = df1.copy();
-output = output.append(df2);
-with pd.ExcelWriter('C:\Users\DELL\Downloads\TESTE_UNIFICAR\output.xlsm') as writer:
- output.to_excel(writer, sheet_name=sheet)
-
-output = pd.read_excel('C:\Users\DELL\Downloads\TESTE_UNIFICAR\output.xlsm', sheet_namename=sheet, index_col=0);
-print("Column headings:");
-print(output.columns);
+	df.to_csv('output.csv', sep=';', index = None, header=True)
+	os.system('cls');
+	print("output.csv was mounted");
+except Exception as e:
+	raise e
